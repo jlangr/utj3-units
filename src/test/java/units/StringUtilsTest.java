66abf89f9bcb80c4b3c662a4908d35152d89264a
Jeff Langr <jeff@langrsoft.com>
2023-07-11 21:02:34 -0600

    fix to highlights

--- src/test/java/units/StringUtilsTest.java
+++ src/test/java/units/StringUtilsTest.java
@@ -1,3 +1,4 @@
+// START:zero
 package units;
 
 import org.junit.jupiter.api.Nested;
@@ -8,23 +9,26 @@ import static units.StringUtils.capitalize;
 public class StringUtilsTest {
    @Nested
    class Capitalize {
-      // START:zero
       @Test
       void returnsEmptyStringWhenEmpty() {
          assertEquals("", capitalize(""));
       }
       // END:zero
 
-      // START:rest
+      // START:one
       @Test
       void returnsUppercasedLetterWhenSingleLetter() {
          assertEquals("A", capitalize("a"));
       }
+      // END:one
 
+      // START:many
       @Test
       void returnsCamelCasedWord() {
          assertEquals("Alpha", capitalize("alpha"));
       }
-      // END:rest
+      // END:many
+      // START:zero
    }
 }
+// END:zero

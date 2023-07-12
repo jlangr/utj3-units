d1ccb6fc3e6109c8af58a305ba13e6c71b507e82
Jeff Langr <jeff@langrsoft.com>
2023-07-11 21:34:17 -0600

    guard clause

--- src/main/java/units/StringUtils.java
+++ src/main/java/units/StringUtils.java
@@ -3,6 +3,10 @@ package units;
 public class StringUtils {
    // START:capitalize
    static String capitalize(String word) {
+      // START_HIGHLIGHT
+      if (word.isEmpty()) return "";
+      // END_HIGHLIGHT
+
       var head = word.substring(0, 1);
       var tail = word.substring(1);
       return head.toUpperCase() + tail.toLowerCase();

624b15c6fc8c13bac81308ce91e146730245d055
Jeff Langr <jeff@langrsoft.com>
2023-07-13 20:53:39 -0600

    location

---
+++ src/main/java/units/Location.java
@@ -0,0 +1,43 @@
+package units;
+
+import java.util.Objects;
+
+public class Location {
+   enum Heading {North, East, South, West}
+
+   private int x, y;
+   private Heading heading;
+
+   public Location(int x, int y, Heading heading) {
+      this.x = x;
+      this.y = y;
+      this.heading = heading;
+   }
+
+   public void move(int distance) {
+      switch (heading) {
+         case North -> y = y + distance;
+         case East -> x = x + distance;
+         case South -> y = y - distance;
+         case West -> x = x - distance;
+      }
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Location location = (Location) o;
+      return x == location.x && y == location.y && heading == location.heading;
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(x, y, heading);
+   }
+
+   @Override
+   public String toString() {
+      return "(" + x + ", " + y + ", => " + heading + ')';
+   }
+}

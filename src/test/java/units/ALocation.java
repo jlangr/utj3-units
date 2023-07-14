3312b2527862e3d70abc14ff59141a04dad71835
Jeff Langr <jeff@langrsoft.com>
2023-07-13 22:14:20 -0600

    test

--- src/test/java/units/ALocation.java
+++ src/test/java/units/ALocation.java
@@ -1,4 +1,18 @@
 package units;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static units.Location.Heading.North;
+
 public class ALocation {
+   @Test
+   void increasesYCoordinateWhenMovingNorth() {
+      var location = new Location(0, 0, North);
+
+      location.move(42);
+
+      assertEquals(0, location.getX());
+      assertEquals(42, location.getY());
+   }
 }

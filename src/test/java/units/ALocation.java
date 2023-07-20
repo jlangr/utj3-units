51e08b25ee1640e9bd7c8664a476e1157c0d79ef
Jeff Langr <jeff@langrsoft.com>
2023-07-20 17:29:42 -0600

    tests for e-s-w

--- src/test/java/units/ALocation.java
+++ src/test/java/units/ALocation.java
@@ -3,7 +3,7 @@ package units;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static units.Location.Heading.North;
+import static units.Location.Heading.*;
 
 public class ALocation {
    @Test
@@ -14,5 +14,41 @@ public class ALocation {
 
       assertEquals(0, location.getX());
       assertEquals(42, location.getY());
+   }
+
+   @Test
+      // START_HIGHLIGHT
+   void increasesXCoordinateWhenMovingEast() {
+      // END_HIGHLIGHT
+      var location = new Location(-2, 0, East);
+
+      location.move(5);
+
+      assertEquals(3, location.getX());
+      assertEquals(0, location.getY());
+   }
+
+   @Test
+      // START_HIGHLIGHT
+   void decreasesYCoordinateWhenMovingSouth() {
+      // END_HIGHLIGHT
+      var location = new Location(-2, 5, South);
+
+      location.move(9);
+
+      assertEquals(-2, location.getX());
+      assertEquals(-4, location.getY());
+   }
+
+   @Test
+   // START_HIGHLIGHT
+   void decreasesXCoordinateWhenMovingWest() {
+   // END_HIGHLIGHT
+      var location = new Location(-2, 5, West);
+
+      location.move(12);
+
+      assertEquals(-14, location.getX());
+      assertEquals(5, location.getY());
    }
 }

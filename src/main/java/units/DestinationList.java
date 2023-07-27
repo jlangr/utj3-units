b9129e1d714833ea948f07d426258448c37e98ca
Jeff Langr <jeff@langrsoft.com>
2023-07-27 18:43:23 -0400

    nonfinal locations field

--- src/main/java/units/DestinationList.java
+++ src/main/java/units/DestinationList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DestinationList {
-   private final List<FixedLocation> locations = new ArrayList<>();
+   private List<FixedLocation> locations = new ArrayList<>();
 
    public void add(FixedLocation location) {
       // START:add

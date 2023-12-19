0c6d21be4b819f4f82552ffd6daf3c2c4b9343b1
Jeff L <jeff@langrsoft.com>
2023-12-19 13:51:34 -0700

    hopefully fix zero part

--- src/test/java/units/StringUtilsTest.java
+++ src/test/java/units/StringUtilsTest.java
@@ -7,28 +7,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static units.StringUtils.capitalize;
 
 public class StringUtilsTest {
+   // END:zero
    @Nested
    class Capitalize {
-      @Test
-      void returnsEmptyStringWhenEmpty() {
-         assertEquals("", capitalize(""));
-      }
-      // END:zero
-
-      // START:one
-      @Test
-      void returnsUppercasedLetterWhenSingleLetter() {
-         assertEquals("A", capitalize("a"));
-      }
-      // END:one
-
-      // START:many
-      @Test
-      void returnsCamelCasedWord() {
-         assertEquals("Deee-lite", capitalize("deee-Lite"));
-      }
-      // END:many
-      // START:zero
+   // START:zero
+   @Test
+   void returnsEmptyStringWhenEmpty() {
+      assertEquals("", capitalize(""));
    }
+   // END:zero
+
+   // START:one
+   @Test
+   void returnsUppercasedLetterWhenSingleLetter() {
+      assertEquals("A", capitalize("a"));
+   }
+   // END:one
+
+   // START:many
+   @Test
+   void returnsCamelCasedWord() {
+      assertEquals("Deee-lite", capitalize("deee-Lite"));
+   }
+   // END:many
+   }
+   // START:zero
 }
 // END:zero

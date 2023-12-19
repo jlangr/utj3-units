3e2877bc86074a4500e42a6d12cf2631a7e6f43b
Jeff L <jeff@langrsoft.com>
2023-12-19 13:54:51 -0700

    fix nested stuff on v1 only

--- src/test/java/units/StringUtilsTest.java
+++ src/test/java/units/StringUtilsTest.java
@@ -1,34 +1,30 @@
 // START:zero
 package units;
 
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static units.StringUtils.capitalize;
 
 public class StringUtilsTest {
-   @Nested
-   class Capitalize {
-      @Test
-      void returnsEmptyStringWhenEmpty() {
-         assertEquals("", capitalize(""));
-      }
-      // END:zero
-
-      // START:one
-      @Test
-      void returnsUppercasedLetterWhenSingleLetter() {
-         assertEquals("A", capitalize("a"));
-      }
-      // END:one
-
-      // START:many
-      @Test
-      void returnsCamelCasedWord() {
-         assertEquals("Alpha", capitalize("alpha"));
-      }
-      // END:many
-      // START:zero
+   @Test
+   void returnsEmptyStringWhenEmpty() {
+      assertEquals("", capitalize(""));
    }
+   // END:zero
+
+   // START:one
+   @Test
+   void returnsUppercasedLetterWhenSingleLetter() {
+      assertEquals("A", capitalize("a"));
+   }
+   // END:one
+
+   // START:many
+   @Test
+   void returnsCamelCasedWord() {
+      assertEquals("Alpha", capitalize("alpha"));
+   }
+   // END:many
+   // START:zero
 }
 // END:zero

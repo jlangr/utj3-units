b16a059423cd0098839a8093b9a20924a4ddfb82
Jeff L <jeff@langrsoft.com>
2024-05-01 13:39:59 -0600

    point

--- src/test/java/units/ALocation.java
+++ src/test/java/units/ALocation.java
@@ -13,8 +13,7 @@ public class ALocation {
 
       location.move(42);
 
-      assertEquals(0, location.getX());
-      assertEquals(42, location.getY());
+      assertEquals(new Point(0, 42), location.getPoint());
    }
 
    @Test
@@ -25,8 +24,7 @@ public class ALocation {
 
       location.move(5);
 
-      assertEquals(3, location.getX());
-      assertEquals(0, location.getY());
+      assertEquals(new Point(3, 0), location.getPoint());
    }
 
    @Test
@@ -37,8 +35,7 @@ public class ALocation {
 
       location.move(9);
 
-      assertEquals(-2, location.getX());
-      assertEquals(-4, location.getY());
+      assertEquals(new Point(-2, -4), location.getPoint());
    }
 
    @Test
@@ -49,8 +46,7 @@ public class ALocation {
 
       location.move(12);
 
-      assertEquals(-14, location.getX());
-      assertEquals(5, location.getY());
+      assertEquals(new Point(-14, 5), location.getPoint());
    }
 }
 // END:newtests
